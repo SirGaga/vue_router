36426fa3a06ddeb6b49caa5e2f29fad9a04e3dc9
family <214151436@qq.com>
2020-10-23 17:49:32 +0800

    'router'

--- src/views/User.vue
+++ src/views/User.vue
@@ -15,6 +15,9 @@ export default {
     userId() {
       // $route 表示当前处于活跃状态的路由
       // $route.params.userId userId代表在路由定义的时候在路由路径上填写的:userId参数名字
+      // 所有的组件都继承自Vue的原型
+      // $router 是VueRouter实例，想要导航到不同URL，则使用$router.push方法
+      // $route 是当前route跳转对象里面可以获取name、path、query、params等
       return this.$route.params.userId;
     }
   }
